6ed4b77932fca0baf964960e3dd7396a389ba1d7
jlecordier <jeanlecordier@hotmail.fr>
2024-09-04 21:46:10 +0200

    tests: api: token: add fetchToken test

---
+++ tests/api/meteofrance/fetchToken.integration.spec.ts
@@ -0,0 +1,10 @@
+import { fetchToken } from '@/api/meteofrance/token/fetchToken.js';
+import { describe, expect, it } from 'vitest';
+
+describe('fetchToken', () => {
+    it('should fetch a new token', async () => {
+        const accessToken = await fetchToken();
+        expect(accessToken).toEqual(expect.any(String));
+        expect(accessToken.length).toBeGreaterThan(0);
+    });
+});
