ef56d9b18308660201bd0fa93983d9e953fc92a4
jlecordier <jeanlecordier@hotmail.fr>
2024-09-04 22:00:15 +0200

    stations: information station: add interface

---
+++ src/stations/information-station/api/InformationStationData.ts
@@ -0,0 +1,32 @@
+export interface InformationStationDataElement {
+    id: number;
+    nom: string;
+    lieuDit: string;
+    bassin: string;
+    dateDebut: string;
+    dateFin: string;
+    typesPoste: {
+        type: number;
+        dateDebut: string;
+        dateFin: string;
+    }[];
+    parametres: {
+        nom: string;
+        dateDebut: string;
+        dateFin: string;
+    }[];
+    producteurs: {
+        nom: string;
+        dateDebut: string;
+        dateFin: string;
+    }[];
+    positions: {
+        altitude: number;
+        latitude: number;
+        longitude: number;
+        dateDebut: string;
+        dateFin: string;
+    }[];
+}
+
+export type InformationStationData = InformationStationDataElement[];
