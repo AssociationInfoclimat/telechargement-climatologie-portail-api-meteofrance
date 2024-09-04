0c0e9b1c91709e0f3c356147ea2fc52432458bb3
jlecordier <jeanlecordier@hotmail.fr>
2024-09-04 21:28:37 +0200

    id station: add value object

---
+++ src/id-station/IdStation.ts
@@ -0,0 +1,26 @@
+export class InvalidIdStationError extends Error {
+    constructor(id: string) {
+        super(
+            `Invalid station id: '${id}'. Must be 8 characters long. nomenclature : 8 chiffres selon DDCCCNNN = insee de la commune (DD département, CCC n° de la commune dans le département et NNN n° de la station dans la commune`
+        );
+    }
+}
+
+export class IdStation {
+    private readonly id: string;
+
+    private constructor(id: string) {
+        this.id = id;
+    }
+
+    static of(id: string): IdStation {
+        if (id.length !== 8) {
+            throw new InvalidIdStationError(id);
+        }
+        return new IdStation(id);
+    }
+
+    value(): string {
+        return this.id;
+    }
+}
