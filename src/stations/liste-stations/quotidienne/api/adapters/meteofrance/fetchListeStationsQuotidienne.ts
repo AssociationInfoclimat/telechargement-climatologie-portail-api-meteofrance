d363c6319fd1119a6c223e2fd1c086d57f80db42
jlecordier <jeanlecordier@hotmail.fr>
2024-09-05 12:13:38 +0200

    stations: liste stations: quotidienne: fetcher: use MF wrapper

--- src/stations/liste-stations/quotidienne/api/adapters/meteofrance/fetchListeStationsQuotidienne.ts
+++ src/stations/liste-stations/quotidienne/api/adapters/meteofrance/fetchListeStationsQuotidienne.ts
@@ -1,25 +1,16 @@
-import { get } from '@/api/api-call.js';
 import { APIResponse, TooManyRetriesError, UnexpectedResponseError } from '@/api/APIResponse.js';
+import { getMF } from '@/api/meteofrance/meteofrance-api-call.js';
 import { TokenStorage } from '@/api/meteofrance/token/TokenStorage.js';
 import { wait } from '@/lib/wait.js';
 import { ListeStationsData } from '@/stations/liste-stations/api/ListeStationsData.js';
 import { Departement } from '@/stations/liste-stations/departements/Departement.js';
 import { z } from 'zod';
 
-export function fetchListeStationsQuotidienne({
-    departement,
-    token,
-}: {
-    departement: Departement;
-    token: string;
-}): Promise<APIResponse<ListeStationsData>> {
-    return get({
+export function fetchListeStationsQuotidienne(departement: Departement): Promise<APIResponse<ListeStationsData>> {
+    return getMF({
         url: `https://public-api.meteofrance.fr/public/DPClim/v1/liste-stations/quotidienne`,
         params: {
             'id-departement': departement.value().toString(),
-        },
-        headers: {
-            Authorization: `Bearer ${token}`,
         },
     });
 }
@@ -29,17 +20,14 @@ export class ListeStationsQuotidienneFetcher {
         departement: Departement,
         { retries = 3 }: { retries?: number } = {}
     ): Promise<ListeStationsData> {
-        const tokenStorage = TokenStorage.getSingleton();
-        const response = await fetchListeStationsQuotidienne({
-            departement,
-            token: await tokenStorage.getToken(),
-        });
+        const response = await fetchListeStationsQuotidienne(departement);
         if (response.code !== 200 && retries === 0) {
             throw new TooManyRetriesError(response);
         }
         if (response.code === 401) {
+            const tokenStorage = TokenStorage.getSingleton();
             await tokenStorage.updateToken();
-            return await this.fetchListeStationsQuotidienne(departement, { retries: retries - 1 });
+            return await this.fetchListeStationsQuotidienne(departement, { retries });
         }
         if ([500, 502].includes(response.code)) {
             await wait(5 * 1000);
