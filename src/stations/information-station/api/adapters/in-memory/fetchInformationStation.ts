836f2c18ac2e148b85f581fe49c3c47c71fc8015
jlecordier <jeanlecordier@hotmail.fr>
2024-09-06 21:52:52 +0200

    stations: information station: api: create in memory api fetcher

---
+++ src/stations/information-station/api/adapters/in-memory/fetchInformationStation.ts
@@ -0,0 +1,44 @@
+import { APIResponse } from '@/api/APIResponse.js';
+import { IdStation } from '@/id-station/IdStation.js';
+import { InformationStationAPIFetcher } from '@/stations/information-station/api/InformationStationAPIFetcher.js';
+
+export function createInMemoryInformationStationAPIFetcher(
+    db: Record<string, APIResponse<unknown>> = {}
+): InformationStationAPIFetcher {
+    const map = new Map(Object.entries(db).map(([key, value]) => [IdStation.of(key).value(), value]));
+    return (idStation: IdStation) => {
+        return Promise.resolve(map.get(idStation.value()) ?? createNotFoundErrorAPIResponse(idStation));
+    };
+}
+
+export function createSuccessfulAPIResponse<T>(data: T): APIResponse<T> {
+    return {
+        code: 200,
+        message: 'OK',
+        data,
+    };
+}
+
+export function createNotFoundErrorAPIResponse(idStation: IdStation): APIResponse<unknown> {
+    return {
+        code: 404,
+        message: `Station '${idStation.value()}' not found`,
+        data: null,
+    };
+}
+
+export function createServerErrorAPIResponse({
+    code = 500,
+    message = '',
+    data = {},
+}: {
+    code?: 500 | 502;
+    message?: string;
+    data?: unknown;
+} = {}): APIResponse<unknown> {
+    return {
+        code,
+        message,
+        data,
+    };
+}
