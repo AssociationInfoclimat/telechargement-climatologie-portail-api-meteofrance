c0025411dc8af48468d09b7291b1e3bae623914b
jlecordier <jeanlecordier@hotmail.fr>
2024-09-06 14:59:49 +0200

    stations: liste stations: db: adapters: add in memory repository

---
+++ src/stations/liste-stations/db/adapters/in-memory/InMemoryStationsRepository.ts
@@ -0,0 +1,22 @@
+import { StationsRepository } from '@/stations/liste-stations/db/StationsRepository.js';
+import { StationDTO, Stations } from '@/stations/liste-stations/Station.js';
+
+export class InMemoryStationsRepository implements StationsRepository {
+    private readonly stations: Stations;
+
+    private constructor(stations: Stations) {
+        this.stations = stations;
+    }
+
+    static of(dtos: StationDTO[] = []): InMemoryStationsRepository {
+        return new InMemoryStationsRepository(Stations.of(dtos));
+    }
+
+    async insert(stations: Stations): Promise<void> {
+        this.stations.merge(stations);
+    }
+
+    selectAll(): Promise<Stations> {
+        return Promise.resolve(this.stations);
+    }
+}
---
+++ tests/stations/liste-stations/db/adapters/in-memory/InMemoryStationsRepository.integration.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryStationsRepository } from '@/stations/liste-stations/db/adapters/in-memory/InMemoryStationsRepository.js';
+import { Stations } from '@/stations/liste-stations/Station.js';
+import { describe, expect, it } from 'vitest';
+
+describe('InMemoryStationsRepository', () => {
+    it('should insert stations', async () => {
+        const stationsToInsert: Stations = Stations.of([
+            {
+                id: '76116001',
+                nom: 'ROUEN-BOOS',
+                departement: 76,
+                frequence: 'infrahoraire-6m',
+                posteOuvert: true,
+                typePoste: 0,
+                lon: 1.178333,
+                lat: 49.3895,
+                alt: 156,
+                postePublic: true,
+            },
+            {
+                id: '76130001',
+                nom: 'BOUELLES',
+                departement: 76,
+                frequence: 'infrahoraire-6m',
+                posteOuvert: true,
+                typePoste: 1,
+                lon: 1.5025,
+                lat: 49.733167,
+                alt: 232,
+                postePublic: true,
+            },
+        ]);
+        const repository = InMemoryStationsRepository.of([]);
+        await repository.insert(stationsToInsert);
+        const insertedStations = await repository.selectAll();
+        expect(insertedStations).toEqual(stationsToInsert);
+    });
+});
