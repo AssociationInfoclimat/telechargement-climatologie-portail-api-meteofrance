f30d32c37f60a5a7a385dd68b89f47eeb3c4e1e3
jlecordier <jeanlecordier@hotmail.fr>
2024-09-07 23:16:13 +0200

    clean code: stations: liste stations: api: replace api response type by unknown

--- src/stations/liste-stations/api/ListeStationsAPIFetcher.ts
+++ src/stations/liste-stations/api/ListeStationsAPIFetcher.ts
@@ -1,5 +1,4 @@
 import { APIResponse } from '@/api/APIResponse.js';
-import { ListeStationsData } from '@/stations/liste-stations/api/ListeStationsData.js';
 import { Departement } from '@/stations/liste-stations/departements/Departement.js';
 
-export type ListeStationsAPIFetcher = (departement: Departement) => Promise<APIResponse<ListeStationsData>>;
+export type ListeStationsAPIFetcher = (departement: Departement) => Promise<APIResponse<unknown>>;
