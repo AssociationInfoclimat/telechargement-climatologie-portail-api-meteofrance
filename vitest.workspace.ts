cc99dd0664a66a9d3d94b7fd3ae6f008a9930e25
jlecordier <jeanlecordier@hotmail.fr>
2024-09-10 20:57:15 +0200

    tests: vitest: add e2e workspace

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -6,7 +6,7 @@ export default defineWorkspace([
         test: {
             name: 'unit',
             include: ['**/*.spec.ts'],
-            exclude: ['**/*.integration.spec.ts'],
+            exclude: ['**/*.integration.spec.ts', '**/*.e2e.spec.ts'],
             sequence: {
                 concurrent: true,
             },
@@ -17,6 +17,14 @@ export default defineWorkspace([
         test: {
             name: 'integration',
             include: ['**/*.integration.spec.ts'],
+            testTimeout: 1 * 60 * 1000,
+        },
+    },
+    {
+        extends: './vitest.config.ts',
+        test: {
+            name: 'e2e',
+            include: ['**/*.e2e.spec.ts'],
             testTimeout: 1 * 60 * 1000,
         },
     },
