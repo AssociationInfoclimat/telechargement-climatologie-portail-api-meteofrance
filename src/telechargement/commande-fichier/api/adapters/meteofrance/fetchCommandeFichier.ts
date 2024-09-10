de6a9fe1fef4bf7c129b8e8326aa43ea1d9e2ed6
jlecordier <jeanlecordier@hotmail.fr>
2024-09-10 19:44:41 +0200

    clean code: telechargement: commande fichier: api: fetchCommandeFichier: use explicit type, convert arrow to anonymous

--- src/telechargement/commande-fichier/api/adapters/meteofrance/fetchCommandeFichier.ts
+++ src/telechargement/commande-fichier/api/adapters/meteofrance/fetchCommandeFichier.ts
@@ -1,14 +1,11 @@
-import { APIResponse } from '@/api/APIResponse.js';
 import { getMF } from '@/api/meteofrance/meteofrance-api-call.js';
-import { CommandeData, CommandeResult } from '@/telechargement/commande-fichier/api/CommandeResult.js';
+import { CommandeFichierAPIFetcher } from '@/telechargement/commande-fichier/api/CommandeFichierAPIFetcher.js';
 
-export function fetchCommandeFichier<T extends CommandeData>(
-    idCommande: string
-): Promise<APIResponse<CommandeResult<T>>> {
+export const fetchCommandeFichier: CommandeFichierAPIFetcher = function (idCommande: string) {
     return getMF({
         url: `https://public-api.meteofrance.fr/public/DPClim/v1/commande/fichier`,
         params: {
             'id-cmde': idCommande,
         },
     });
-}
+};
