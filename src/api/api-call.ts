a117260a00fe0f724863a01dcefccd33fb176d2b
jlecordier <jeanlecordier@hotmail.fr>
2024-09-13 14:15:45 +0200

    api: api call: add low level retry mechanism

--- src/api/api-call.ts
+++ src/api/api-call.ts
@@ -1,4 +1,49 @@
+import { APIResponse } from '@/api/APIResponse.js';
 import { getUsingAxios, postUsingAxios } from '@/api/Axios.js';
+import { LoggerSingleton } from '@/lib/logger/LoggerSingleton.js';
 
-export const get = getUsingAxios;
-export const post = postUsingAxios;
+export async function get(
+    {
+        url,
+        params,
+        headers,
+    }: {
+        url: string;
+        params: Record<string, string>;
+        headers?: { Authorization?: string };
+    },
+    { retries = 3 }: { retries?: number } = {}
+): Promise<APIResponse> {
+    try {
+        return await getUsingAxios({ url, params, headers });
+    } catch (error) {
+        LoggerSingleton.getSingleton().warn({ data: error });
+        if (retries <= 0) {
+            throw error;
+        }
+        return await get({ url, params, headers }, { retries: retries - 1 });
+    }
+}
+
+export async function post(
+    {
+        url,
+        body,
+        headers,
+    }: {
+        url: string;
+        body: Record<string, string>;
+        headers?: { Authorization?: string };
+    },
+    { retries = 3 }: { retries?: number } = {}
+): Promise<APIResponse> {
+    try {
+        return await postUsingAxios({ url, body, headers });
+    } catch (error) {
+        LoggerSingleton.getSingleton().warn({ data: error });
+        if (retries <= 0) {
+            throw error;
+        }
+        return await post({ url, body, headers }, { retries: retries - 1 });
+    }
+}
