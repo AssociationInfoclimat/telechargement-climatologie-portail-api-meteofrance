adfc2787af3c7d2b37c4679b7946db2575aec71a
jlecordier <jeanlecordier@hotmail.fr>
2024-09-30 17:41:52 +0200

    fix: tests: telechargement: commande fichier: api: fetchCommandeFichier: increase commande size to make it pending

--- tests/telechargement/commande-fichier/api/adapters/meteofrance/fetchCommandeFichier.integration.spec.ts
+++ tests/telechargement/commande-fichier/api/adapters/meteofrance/fetchCommandeFichier.integration.spec.ts
@@ -26,7 +26,7 @@ describe('CommandeFichierFetcher', () => {
                     idStation: IdStation.of('76116001'),
                     periodeCommande: PeriodeCommande.of({
                         debut: '2024-06-15T12:00:00Z',
-                        fin: '2024-06-17T14:00:00Z',
+                        fin: '2024-06-17T15:00:00Z',
                     }),
                 });
                 const fetcher = new CommandeFichierFetcher({ commandeFichierAPIFetcher: fetchCommandeFichier });
