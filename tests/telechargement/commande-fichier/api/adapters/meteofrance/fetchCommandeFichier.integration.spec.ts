bafe6fabfaec560ffbcd786b78a078c52e085fa5
jlecordier <jeanlecordier@hotmail.fr>
2024-11-04 14:33:28 +0100

    tests: telechargement: fetchCommandeFichier: increase date range to make it pending

--- tests/telechargement/commande-fichier/api/adapters/meteofrance/fetchCommandeFichier.integration.spec.ts
+++ tests/telechargement/commande-fichier/api/adapters/meteofrance/fetchCommandeFichier.integration.spec.ts
@@ -26,7 +26,7 @@ describe('CommandeFichierFetcher', () => {
                     idStation: IdStation.of('76116001'),
                     periodeCommande: PeriodeCommande.of({
                         debut: '2024-06-15T12:00:00Z',
-                        fin: '2024-06-17T17:00:00Z',
+                        fin: '2024-06-17T20:00:00Z',
                     }),
                 });
                 const fetcher = new CommandeFichierFetcher({ commandeFichierAPIFetcher: fetchCommandeFichier });
@@ -65,7 +65,7 @@ describe('CommandeFichierFetcher', () => {
                     idStation: IdStation.of('76116001'),
                     periodeCommande: PeriodeCommande.of({
                         debut: '2024-06-15T12:00:00Z',
-                        fin: '2024-06-16T14:00:00Z',
+                        fin: '2024-06-16T15:00:00Z',
                     }),
                 });
                 const fetcher = new CommandeFichierFetcher({ commandeFichierAPIFetcher: fetchCommandeFichier });
@@ -104,7 +104,7 @@ describe('CommandeFichierFetcher', () => {
                     idStation: IdStation.of('76116001'),
                     periodeCommande: PeriodeCommande.of({
                         debut: '2024-06-15T12:00:00Z',
-                        fin: '2024-07-14T12:00:00Z',
+                        fin: '2024-07-15T12:00:00Z',
                     }),
                 });
                 const fetcher = new CommandeFichierFetcher({ commandeFichierAPIFetcher: fetchCommandeFichier });
