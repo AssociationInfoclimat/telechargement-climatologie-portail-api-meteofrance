31be7a224cdc7f1ee9826e6885e5ed7d5296f1f0
jlecordier <jeanlecordier@hotmail.fr>
2025-06-03 16:52:26 +0200

    vitest: move workspace config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,34 @@ export default defineConfig({
             '@/': new URL('./src/', import.meta.url).pathname,
         },
         setupFiles: [new URL('./vitest.setup.ts', import.meta.url).pathname],
+        projects: [
+            {
+                extends: true,
+                test: {
+                    name: 'unit',
+                    include: ['**/*.spec.ts'],
+                    exclude: ['**/*.integration.spec.ts', '**/*.e2e.spec.ts'],
+                    sequence: {
+                        concurrent: true,
+                    },
+                },
+            },
+            {
+                extends: true,
+                test: {
+                    name: 'integration',
+                    include: ['**/*.integration.spec.ts'],
+                    testTimeout: 1 * 60 * 1000,
+                },
+            },
+            {
+                extends: true,
+                test: {
+                    name: 'e2e',
+                    include: ['**/*.e2e.spec.ts'],
+                    testTimeout: 1 * 60 * 1000,
+                },
+            },
+        ],
     },
 });
