03e4b17355575ee9ac63abbcd5b9cd68977f5d47
jlecordier <jeanlecordier@hotmail.fr>
2025-06-10 15:55:42 +0200

    fix: getDepartements: add Andorre option

--- src/stations/liste-stations/departements/getDepartements.ts
+++ src/stations/liste-stations/departements/getDepartements.ts
@@ -1,11 +1,13 @@
 import { Departement } from '@/stations/liste-stations/departements/Departement.js';
 
-export function getDepartements(): Departement[] {
+export function getDepartements(includeAndorre: boolean = true): Departement[] {
     const ids = [];
     for (let i = 1; i <= 95; i++) {
         ids.push(Departement.of(i));
     }
-    ids.push(Departement.of(99));
+    if (includeAndorre) {
+        ids.push(Departement.of(99));
+    }
     for (let i = 971; i <= 975; i++) {
         ids.push(Departement.of(i));
     }
